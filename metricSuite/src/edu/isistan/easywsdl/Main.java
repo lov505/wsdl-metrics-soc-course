ce47b0dfe4ec20878e9d0cc68889811a088118ee
mcrasso <mcrasso@f930311f-6cf5-1e8f-470e-355331a7b681>
2011-12-14 21:14:30 +0000

    documentacion y actualizacion output

--- metricSuite/src/edu/isistan/easywsdl/Main.java
+++ metricSuite/src/edu/isistan/easywsdl/Main.java
@@ -8,17 +8,26 @@ import org.apache.log4j.Logger;
 public class Main {
 
 	/**
+	 * Outputs to standard output a comma separated file according to URL,DW,DMR,ME,MRS
 	 * @param args
 	 */
-	public static void main(String[] args) {		
+	public static void main(String[] args) {	
+		System.out.println("URL,DW,DMR,ME,MRS");
 		for (int j = 0; j < args.length; j++) {
 			String descUrl = args[j];			
 			try {
-				MetricsSuite ms = new MetricsSuite();
-				System.out.println( ms.getDW( URI.create(descUrl).toURL()) );
-				System.out.println( ms.getDMR( URI.create(descUrl).toURL()) );
-				System.out.println( ms.getME( URI.create(descUrl).toURL()) );
-				System.out.println( ms.getMRS( URI.create(descUrl).toURL()) );
+				MetricsSuite ms = new MetricsSuite();				
+				StringBuffer sb = new StringBuffer();
+				sb.append(descUrl);
+				sb.append(",");
+				sb.append( ms.getDW( URI.create(descUrl).toURL()) );
+				sb.append(",");
+				sb.append( ms.getDMR( URI.create(descUrl).toURL()) );
+				sb.append(",");
+				sb.append( ms.getME( URI.create(descUrl).toURL()) );
+				sb.append(",");
+				sb.append( ms.getMRS( URI.create(descUrl).toURL()) );
+				System.out.println(sb.toString());
 			} catch (MalformedURLException e) {
 				Logger.getLogger(MessageComplexityCalculator.class.getName()).error("Malformed: " + descUrl,e);
 			}
