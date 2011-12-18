cc7b04a88002540f9099015bd875033d75511f11
mcrasso <mcrasso@f930311f-6cf5-1e8f-470e-355331a7b681>
2011-12-18 14:20:18 +0000

    parametros

--- metricSuite/run.sh
+++ metricSuite/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp bin/:lib/easywsdl-schema-2.1.jar:lib/easywsdl-wsdl-2.1.jar:lib/log4j-1.2.16.jar edu.isistan.easywsdl.Main $1
+java -cp bin/:lib/easywsdl-schema-2.1.jar:lib/easywsdl-wsdl-2.1.jar:lib/log4j-1.2.16.jar edu.isistan.easywsdl.Main $@
