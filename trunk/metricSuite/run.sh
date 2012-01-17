ece70ffa0c9fac804018b50023522237897b7603
mgarriga87@gmail.com <mgarriga87@gmail.com@f930311f-6cf5-1e8f-470e-355331a7b681>
2012-01-17 14:25:05 +0000

    Ajustes en calculo de restrictions. Agregado run.bat

--- trunk/metricSuite/run.sh
+++ trunk/metricSuite/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-METRICSUITE_HOME=/home/mcrasso/Desktop/teaching/courses/COS/cos2011/final/mgarriga/wsdl-metrics-soc-course/metricSuite
+#METRICSUITE_HOME=/home/mcrasso/Desktop/teaching/courses/COS/cos2011/final/mgarriga/wsdl-metrics-soc-course/metricSuite
 java -cp $METRICSUITE_HOME/bin/:lib/easywsdl-schema-2.1.jar:$METRICSUITE_HOME/lib/easywsdl-wsdl-2.1.jar:$METRICSUITE_HOME/lib/log4j-1.2.16.jar edu.isistan.easywsdl.Main $@
