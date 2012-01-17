04fb5b11a3f8b6273e76c44279b028b4278c5446
mgarriga87@gmail.com <mgarriga87@gmail.com@f930311f-6cf5-1e8f-470e-355331a7b681>
2012-01-17 14:29:58 +0000

    Ejecutar desde cmd en Windows

---
+++ trunk/metricSuite/run.bat
@@ -0,0 +1,1 @@
+java -cp %METRICSUITE_HOME%\bin\;%METRICSUITE_HOME%\lib\easywsdl-schema-2.1.jar;%METRICSUITE_HOME%\lib\easywsdl-wsdl-2.1.jar;%METRICSUITE_HOME%\lib\log4j-1.2.16.jar edu.isistan.easywsdl.Main %1
